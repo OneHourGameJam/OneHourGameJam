bbfec81c683f53f3454991325a303f11fcc6da61
WetDesertRock <wetdesertrock@gmail.com>
2018-10-24 06:44:15 -0600

    Add config SQL to main 1hgj.sql file

--- SQL/1hgj.sql
+++ SQL/1hgj.sql
@@ -406,6 +406,21 @@ CREATE TABLE `user` (
   `user_role` int(11) DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE TABLE `config` (
+  `config_id` int(11) NOT NULL,
+  `config_lastedited` datetime NOT NULL,
+  `config_lasteditedby` varchar(45) NOT NULL,
+  `config_key` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `config_value` mediumtext COLLATE utf8mb4_bin NOT NULL,
+  `config_category` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `config_description` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `config_type` mediumtext COLLATE utf8mb4_bin NOT NULL,
+  `config_options` mediumtext COLLATE utf8mb4_bin NOT NULL,
+  `config_editable` tinyint(1) NOT NULL,
+  `config_required` tinyint(1) NOT NULL,
+  `config_template` tinyint(1) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
 --
 -- Indexes for dumped tables
 --
@@ -484,6 +499,13 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `user_username_UNIQUE` (`user_username`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `config`
+  ADD PRIMARY KEY (`config_id`),
+  ADD UNIQUE KEY `config_key_UNIQUE` (`config_key`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -553,6 +575,13 @@ ALTER TABLE `themevote`
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1408;
 COMMIT;
+
+--
+-- AUTO_INCREMENT for table `config`
+--
+ALTER TABLE `config`
+MODIFY `config_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
