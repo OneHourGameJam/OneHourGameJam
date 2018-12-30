0c23e0bafcdaab38aa0aa7d3b59c621e8e76252f
LiamLime <contact@liamlime.com>
2018-12-30 03:58:32 +0000

    - Adds table creation of admin_log and admin_vote to migration script

--- SQL/migrations/03_adminLogAdminVotesConfig.sql
+++ SQL/migrations/03_adminLogAdminVotesConfig.sql
@@ -1,3 +1,44 @@
+-- Add admin_log and admin_vote tables
+
+--
+-- Table structure for table `admin_log`
+--
+
+DROP TABLE IF EXISTS `admin_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin_log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `log_datetime` datetime DEFAULT NULL,
+  `log_ip` varchar(45) DEFAULT NULL,
+  `log_user_agent` varchar(255) DEFAULT NULL,
+  `log_admin_username` varchar(255) DEFAULT NULL,
+  `log_subject_username` varchar(255) DEFAULT NULL,
+  `log_type` varchar(45) DEFAULT NULL,
+  `log_content` text,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=84 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `admin_vote`
+--
+
+DROP TABLE IF EXISTS `admin_vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `admin_vote` (
+  `vote_id` int(11) NOT NULL AUTO_INCREMENT,
+  `vote_datetime` datetime DEFAULT NULL,
+  `vote_ip` varchar(45) DEFAULT NULL,
+  `vote_user_agent` varchar(255) DEFAULT NULL,
+  `vote_voter_username` varchar(255) DEFAULT NULL,
+  `vote_subject_username` varchar(255) DEFAULT NULL,
+  `vote_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`vote_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
 -- configuration renames column "config_template" to "config_added_to_dictionary"
 ALTER TABLE config 
 CHANGE COLUMN config_template config_added_to_dictionary TINYINT(1) NOT NULL;
