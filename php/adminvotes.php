80e6a25bcf840c024e0ffd8651e6bff4081d0e29
LiamLime <contact@liamlime.com>
2019-02-16 02:06:17 +0000

    - Splits polls.php into polls.php, adminvotes.php and satisfaction.php

---
+++ php/adminvotes.php
@@ -0,0 +1,98 @@
+<?php
+
+function LoadAdminVotes(){
+	global $dbConn;
+	AddActionLog("LoadAdminVotes");
+	StartTimer("LoadAdminVotes");
+
+	$adminVotes = Array();
+
+	$sql = "
+		SELECT v.vote_subject_username, v.vote_type
+		FROM admin_vote v, user u
+        WHERE v.vote_voter_username = u.user_username
+          AND u.user_role = 1
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	while($info = mysqli_fetch_array($data)){
+		$adminVoteData = Array();
+
+		$adminVoteData["subject_username"] = $info["vote_subject_username"];
+		$adminVoteData["vote_type"] = $info["vote_type"];
+
+		$adminVotes[] = $adminVoteData;
+	}
+
+	StopTimer("LoadAdminVotes");
+	return $adminVotes;
+}
+
+function LoadLoggedInUsersAdminVotes(&$loggedInUser){
+	global $dbConn;
+	AddActionLog("LoadLoggedInUsersAdminVotes");
+	StartTimer("LoadLoggedInUsersAdminVotes");
+
+	$loggedInUserAdminVotes = Array();
+
+	$escapedVoterUsername = mysqli_real_escape_string($dbConn, $loggedInUser["username"]);
+
+	$sql = "
+		SELECT vote_subject_username, vote_type
+		FROM admin_vote
+		WHERE vote_voter_username = '$escapedVoterUsername';
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	while($info = mysqli_fetch_array($data)){
+		$adminVoteData = Array();
+
+		$adminVoteData["subject_username"] = $info["vote_subject_username"];
+		$adminVoteData["vote_type"] = $info["vote_type"];
+
+		$loggedInUserAdminVotes[] = $adminVoteData;
+    }
+
+	StopTimer("LoadLoggedInUsersAdminVotes");
+	return $loggedInUserAdminVotes;
+}
+
+function GetAdminVotesCastByUserFormatted($username){
+	global $dbConn;
+	AddActionLog("GetAdminVotesCastByUserFormatted");
+	StartTimer("GetAdminVotesCastByUserFormatted");
+
+	$escapedUsername = mysqli_real_escape_string($dbConn, $username);
+	$sql = "
+		SELECT *
+		FROM admin_vote
+		WHERE vote_voter_username = '$escapedUsername';
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	StopTimer("GetAdminVotesCastByUserFormatted");
+	return ArrayToHTML(MySQLDataToArray($data));
+}
+
+function GetAdminVotesForSubjectUserFormatted($username){
+	global $dbConn;
+	AddActionLog("GetAdminVotesForSubjectUserFormatted");
+	StartTimer("GetAdminVotesForSubjectUserFormatted");
+
+	$escapedUsername = mysqli_real_escape_string($dbConn, $username);
+	$sql = "
+        SELECT vote_id, vote_datetime, 'redacted' AS vote_voter_username, vote_subject_username, 'redacted' AS vote_type
+        FROM admin_vote
+        WHERE vote_subject_username = '$escapedUsername';
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	StopTimer("GetAdminVotesForSubjectUserFormatted");
+	return ArrayToHTML(MySQLDataToArray($data));
+}
+
+?>
---
+++ php/satisfaction.php
@@ -0,0 +1,133 @@
+<?php
+
+function LoadSatisfaction(&$config){
+	global $dbConn;
+	AddActionLog("LoadSatisfaction");
+	StartTimer("LoadSatisfaction");
+
+	$satisfaction = Array();
+
+	$sql = "
+		SELECT
+			satisfaction_question_id,
+			AVG(satisfaction_score) AS average_score,
+			COUNT(satisfaction_score) AS submitted_scores
+		FROM satisfaction
+		GROUP BY satisfaction_question_id
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	//Get data
+	while($satisfactionData = mysqli_fetch_array($data)){
+		$satisfaction = Array();
+
+		$questionId = $satisfactionData["satisfaction_question_id"];
+		$averageScore = $satisfactionData["average_score"];
+		$submittedScores = $satisfactionData["submitted_scores"];
+
+		$satisfaction["question_id"] = $questionId;
+		$satisfaction["average_score"] = $averageScore;
+		$satisfaction["submitted_scores"] = $submittedScores;
+		$satisfaction["enough_scores_to_show_satisfaction"] = $submittedScores >= $config["SATISFACTION_RATINGS_TO_SHOW_SCORE"]["VALUE"];
+
+		$satisfaction[$questionId] = $satisfaction;
+	}
+
+	$sql = "
+		SELECT
+			satisfaction_question_id,
+			satisfaction_score,
+			COUNT(1) AS votes_for_score
+		FROM satisfaction
+		GROUP BY satisfaction_question_id, satisfaction_score
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	//Get data
+	while($info = mysqli_fetch_array($data)){
+		$questionId = $info["satisfaction_question_id"];
+		$satisfactionScore = $info["satisfaction_score"];
+		$votesForScore = $info["votes_for_score"];
+		
+		for($score = -5; $score <= 5; $score++){
+			if(!isset($satisfaction[$questionId]["scores"][$score])){
+				$satisfaction[$questionId]["scores"][$score] = 0;
+			}
+		}
+
+		$satisfaction[$questionId]["scores"][$satisfactionScore] = $votesForScore;
+	}
+
+	StopTimer("LoadSatisfaction");
+	return $satisfaction;
+}
+
+function SubmitSatisfaction(&$loggedInUser, $satisfactionQuestionId, $score){
+	global $dbConn, $ip, $userAgent;
+	AddActionLog("SubmitSatisfaction");
+	StartTimer("SubmitSatisfaction");
+
+	if($score < -5){
+		AddDataWarning("Invalid satisfaction score", false);
+		StopTimer("SubmitSatisfaction");
+		return;
+	}
+	if($score > 5){
+		AddDataWarning("Invalid satisfaction score", false);
+		StopTimer("SubmitSatisfaction");
+		return;
+	}
+
+	$username = trim($loggedInUser["username"]);
+
+	$escapedSatisfactionQuestionId = mysqli_real_escape_string($dbConn, $satisfactionQuestionId);
+	$escapedIP = mysqli_real_escape_string($dbConn, $ip);
+	$escapedUserAgent = mysqli_real_escape_string($dbConn, $userAgent);
+	$escapedUsername = mysqli_real_escape_string($dbConn, $username);
+	$escapedScore = mysqli_real_escape_string($dbConn, $score);
+
+	$sql = "
+		INSERT INTO satisfaction
+		(satisfaction_id,
+		satisfaction_datetime,
+		satisfaction_ip,
+		satisfaction_user_agent,
+		satisfaction_question_id,
+		satisfaction_username,
+		satisfaction_score)
+		VALUES
+		(null,
+		Now(),
+		'$escapedIP',
+		'$escapedUserAgent',
+		'$escapedSatisfactionQuestionId',
+		'$escapedUsername',
+		'$escapedScore');";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	AddDataSuccess("Satisfaction score submitted", false);
+	StopTimer("SubmitSatisfaction");
+}
+
+function GetSatisfactionVotesOfUserFormatted($username){
+	global $dbConn;
+	AddActionLog("GetSatisfactionVotesOfUserFormatted");
+	StartTimer("GetSatisfactionVotesOfUserFormatted");
+
+	$escapedUsername = mysqli_real_escape_string($dbConn, $username);
+	$sql = "
+		SELECT *
+		FROM satisfaction
+		WHERE satisfaction_username = '$escapedUsername';
+	";
+	$data = mysqli_query($dbConn, $sql);
+	$sql = "";
+
+	StopTimer("GetSatisfactionVotesOfUserFormatted");
+	return ArrayToHTML(MySQLDataToArray($data));
+}
+
+?>
