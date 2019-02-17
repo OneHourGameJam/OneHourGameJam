6529731223e87d28e26809c0bcd0ceae799a476d
LiamLime <contact@liamlime.com>
2019-02-17 03:20:49 +0000

    - Removes testing stuff from index.php

--- index.php
+++ index.php
@@ -8,18 +8,15 @@
 include_once("php/site.php");
 StartTimer("index.php");
 
-$dictionary["a"] = "POTATO";
-
-$templateFile = $templateBasePath.$pageSettings[$page]["template_file"];
-
+$pageTemplateFile = $templateBasePath.$pageSettings[$page]["template_file"];
 $mustache->setPartials(Array(
 	"header" => file_get_contents($templateBasePath."header.html"),
 	"message" => file_get_contents($templateBasePath."message.html"),
 	"menu" => file_get_contents($templateBasePath."menu.html"),
 	"footer" => file_get_contents($templateBasePath."footer.html"),
-	"page" => file_get_contents($templateFile),
 	"poll" => file_get_contents($templateBasePath."poll.html"),
 	"notification" => file_get_contents($templateBasePath."notification.html"),
+	"page" => file_get_contents($pageTemplateFile),
 ));
 
 print $mustache->render(file_get_contents($templateBasePath."index.html"), $dictionary);
