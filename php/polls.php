5b7428b391806f542e90cf336129b4f75a37d5f2
LiamLime <contact@liamlime.com>
2019-04-19 22:07:43 +0100

    Adds charts to the polls display page

--- php/polls.php
+++ php/polls.php
@@ -59,6 +59,61 @@ function RenderPolls(&$pollData){
 				$poll["OPTIONS"][$i]["PERCENTAGE_OF_USERS_VOTES_DISPLAY"] = (intval($optionPercentage * 100)) . "%";
 			}
 		}
+		
+
+		$jsFormattedPollOptionsList = Array();
+		$jsFormattedPollVotesList = Array();
+		$jsFormattedPollFillColorList = Array();
+		$jsFormattedPollBorderColorList = Array();
+		$jsFormattedPollUserPercentagesList = Array();
+
+		//Format for JavaScript charts
+		foreach($pollModel->Options as $optionID => $optionModel){
+			$votes = $optionModel->Votes;
+			$percentage = 0;
+			if($totalVotes > 0){
+				$percentage = $votes / $totalVotes;
+			}
+			$userVotesPercentage = 0;
+			if($usersVotedInPoll > 0){
+				$userVotesPercentage = $votes / $usersVotedInPoll;
+			}
+			
+			$optionText = $optionModel->Text;
+
+			$listEntryId = $votes;
+			$safety = 100;
+			while(true){
+				$listEntryId += 0.0001;
+				$safety -= 1;
+				if(!isset($jsFormattedPollOptionsList["".$listEntryId])){
+					break;
+				}
+				if($safety <= 0){
+					break;
+				}
+			}
+
+			$jsFormattedPollOptionsList["".$listEntryId] = "\"".str_replace("\"", "\\\"", htmlspecialchars_decode($optionText, ENT_COMPAT | ENT_HTML401 | ENT_QUOTES))."\"";
+			$jsFormattedPollVotesList["".$listEntryId] = $votes;
+			$jsFormattedPollUserPercentagesList["".$listEntryId] = intval($userVotesPercentage * 100)/100;
+			
+			$randomR = 0x10 + (rand(0,14) * 0x10);
+			$randomG = 0x10 + (rand(0,14) * 0x10);
+			$randomB = 0x10 + (rand(0,14) * 0x10);
+			$jsFormattedPollFillColorList["".$listEntryId] = "'rgba(".$randomR.", ".$randomG.", ".$randomB.", 0.2)'";
+			$jsFormattedPollBorderColorList["".$listEntryId] = "'rgba(".$randomR.", ".$randomG.", ".$randomB.", 1)'";
+		}
+		krsort($jsFormattedPollOptionsList);
+		krsort($jsFormattedPollVotesList);
+		krsort($jsFormattedPollFillColorList);
+		krsort($jsFormattedPollBorderColorList);
+		krsort($jsFormattedPollUserPercentagesList);
+		$poll["js_formatted_options_list"] = implode(",", $jsFormattedPollOptionsList);
+		$poll["js_formatted_votes_list"] = implode(",", $jsFormattedPollVotesList);
+		$poll["js_formatted_fill_color_list"] = implode(",", $jsFormattedPollFillColorList);
+		$poll["js_formatted_border_color_list"] = implode(",", $jsFormattedPollBorderColorList);
+		$poll["js_formatted_user_votes_percentage_list"] = implode(",", $jsFormattedPollUserPercentagesList);
 			
 		if($poll["IS_ACTIVE"]){
 			$render["ACTIVE_POLLS"][] = $poll;
