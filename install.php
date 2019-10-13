edff176ba384656c5abc5a14e8a015091c8feeec
WetDesertRock <wetdesertrock@gmail.com>
2019-10-13 00:00:37 -0600

    Update indentation to tabs (appears to be standard in this project)

--- install.php
+++ install.php
@@ -6,15 +6,15 @@ include_once("php/install_page.php");
 $dictionary = array();
 
 $dictionary['page'] = array(
-    "template_path" => $templateBasePath
+	"template_path" => $templateBasePath
 );
 
 RunInstallPage($dictionary);
 $pageTemplateFile = $dictionary["template_file"];
 $mustache->setPartials(Array(
-    "footer" => file_get_contents($templateBasePath."footer.html"),
-    "message" => file_get_contents($templateBasePath."message.html"),
-    "page" => file_get_contents($templateBasePath.$pageTemplateFile),
+	"footer" => file_get_contents($templateBasePath."footer.html"),
+	"message" => file_get_contents($templateBasePath."message.html"),
+	"page" => file_get_contents($templateBasePath.$pageTemplateFile),
 ));
 
 print $mustache->render(file_get_contents($templateBasePath."install.html"), $dictionary);
--- php/install_page.php
+++ php/install_page.php
@@ -3,93 +3,93 @@
 $dbConfigPath = "config/dbconfig.php";
 
 function RunInstallPage(&$dictionary) {
-    global $dbConfigPath;
+	global $dbConfigPath;
 
-    // Safety check, make sure that this is a fresh installation
-    if (file_exists($dbConfigPath)) {
-        $dictionary['error_message'] = "Database has been configured already! Please delete the dbconfig.php file to start the installation process.";
-        $dictionary['template_file'] = 'install_fail.html';
-        return;
-    }
+	// Safety check, make sure that this is a fresh installation
+	if (file_exists($dbConfigPath)) {
+		$dictionary['error_message'] = "Database has been configured already! Please delete the dbconfig.php file to start the installation process.";
+		$dictionary['template_file'] = 'install_fail.html';
+		return;
+	}
 
-    // Check to see if this is a setup post request
-    if(isset($_POST["action"])) {
-        if ($_POST["action"] == "setup") {
-            // Run the database setup, and then set the template file based on if there was an error or not.
-            $errorMessage = RunSetupAction();
-            $dictionary['error_message'] = $errorMessage;
+	// Check to see if this is a setup post request
+	if(isset($_POST["action"])) {
+		if ($_POST["action"] == "setup") {
+			// Run the database setup, and then set the template file based on if there was an error or not.
+			$errorMessage = RunSetupAction();
+			$dictionary['error_message'] = $errorMessage;
 
-            if ($errorMessage == "") {
-                $dictionary['template_file'] = 'install_success.html';
-                return;
-            } else {
-                $dictionary['template_file'] = 'install_fail.html';
-                return;
-            }
-        }
-    }
-    
-    // Assume that if we got here we want to display the form.
-    $dictionary['template_file'] = 'install_form.html';
+			if ($errorMessage == "") {
+				$dictionary['template_file'] = 'install_success.html';
+				return;
+			} else {
+				$dictionary['template_file'] = 'install_fail.html';
+				return;
+			}
+		}
+	}
+	
+	// Assume that if we got here we want to display the form.
+	$dictionary['template_file'] = 'install_form.html';
 }
 
 // Runs the setup and returns any errors encountered
 function RunSetupAction() {
-    global $dbConfigPath;
+	global $dbConfigPath;
 
-    $dbHost = $_POST["dbHost"];
-    $dbUser = $_POST["dbUsername"];
-    $dbPassword = $_POST["dbPassword"];
-    $dbName = $_POST["dbName"];
+	$dbHost = $_POST["dbHost"];
+	$dbUser = $_POST["dbUsername"];
+	$dbPassword = $_POST["dbPassword"];
+	$dbName = $_POST["dbName"];
 
-    // Check connection
-    $conn = @mysqli_connect($dbHost, $dbUser, $dbPassword);
+	// Check connection
+	$conn = @mysqli_connect($dbHost, $dbUser, $dbPassword);
 
-    if (!$conn) {
-        return "Connection failed: " . mysqli_connect_error();
-    }
+	if (!$conn) {
+		return "Connection failed: " . mysqli_connect_error();
+	}
 
-    // Create the database if needed
-    $escapedDBName = mysqli_real_escape_string($conn, $dbName);
-    $sql = "CREATE DATABASE IF NOT EXISTS $escapedDBName;";
-    if (@mysqli_query($conn, $sql) == FALSE) {
-        return "Unable to create/verify database: " . mysqli_error($conn);
-    }
-    if (mysqli_select_db($conn, $dbName) == FALSE) {
-        return "Unable to select database: " . mysqli_error($conn);
-    }
+	// Create the database if needed
+	$escapedDBName = mysqli_real_escape_string($conn, $dbName);
+	$sql = "CREATE DATABASE IF NOT EXISTS $escapedDBName;";
+	if (@mysqli_query($conn, $sql) == FALSE) {
+		return "Unable to create/verify database: " . mysqli_error($conn);
+	}
+	if (mysqli_select_db($conn, $dbName) == FALSE) {
+		return "Unable to select database: " . mysqli_error($conn);
+	}
 
-    // Run the installation script
-    $sql = file_get_contents("SQL/1hgj.sql");
-    if(mysqli_multi_query($conn, $sql)) {
-        do {
-            mysqli_next_result($conn);
-        }
-        while(mysqli_more_results($conn));
-    }
+	// Run the installation script
+	$sql = file_get_contents("SQL/1hgj.sql");
+	if(mysqli_multi_query($conn, $sql)) {
+		do {
+			mysqli_next_result($conn);
+		}
+		while(mysqli_more_results($conn));
+	}
 
-    if(mysqli_errno($conn)) {
-        return "Unable to run installation script: " . mysqli_error($conn);
-    }
-    
-    
-    // Save credentials in the config file
-    $safeHost = addslashes($dbHost);
-    $safeUser = addslashes($dbUser);
-    $safePassword = addslashes($dbPassword);
-    $safeName = addslashes($dbName);
+	if(mysqli_errno($conn)) {
+		return "Unable to run installation script: " . mysqli_error($conn);
+	}
+	
+	
+	// Save credentials in the config file
+	$safeHost = addslashes($dbHost);
+	$safeUser = addslashes($dbUser);
+	$safePassword = addslashes($dbPassword);
+	$safeName = addslashes($dbName);
 
-    $configData = "<?php
+	$configData = "<?php
 \$dbAddress = \"$safeHost\";
 \$dbUsername = \"$safeUser\";
 \$dbPassword = \"$safePassword\";
 \$dbDatabaseName = \"$safeName\";
 ?>";
-    
-    $result = file_put_contents($dbConfigPath, $configData);
-    if ($result == FALSE) {
-        return "Unable to write config file. Please manually fill it out (database has been initialized)";
-    }
-    
-    return "";
+	
+	$result = file_put_contents($dbConfigPath, $configData);
+	if ($result == FALSE) {
+		return "Unable to write config file. Please manually fill it out (database has been initialized)";
+	}
+	
+	return "";
 }
