bc735a9d4c4b3fb131e8ce1c1f576ffc580eb443
LiamLime <contact@liamlime.com>
2020-03-28 22:50:42 +0000

    - Updates Twitch Stream integration: Now uses twitch's Helix API

--- php/stream.php
+++ php/stream.php
@@ -5,6 +5,7 @@ function InitStream(&$configData){
 	StartTimer("InitStream");
 
 	$render = Array();
+	$data = Array();
 
 	$timeDiff = 0;
 	if(file_exists("cache/twitch_stream.json")){
@@ -21,7 +22,7 @@ function InitStream(&$configData){
 		file_put_contents("cache/twitch_stream.json", json_encode($data));
 
 		//Fetch API response using CURL, because that was the easiest to copy-paste in :)
-		$channelsApi = 'https://api.twitch.tv/kraken/streams/';
+		$channelsApi = 'https://api.twitch.tv/helix/streams?user_login=';
 		$channelName = $configData->ConfigModels["STREAMER_TWITCH_NAME"]->Value;
 		$clientId = $configData->ConfigModels["TWITCH_CLIENT_ID"]->Value;
 		$ch = curl_init();
@@ -43,9 +44,11 @@ function InitStream(&$configData){
 		file_put_contents("cache/twitch_stream.json", json_encode($data));
 	}
 
-	if(isset($data) && isset($data["stream"]) && $data["stream"] != null && count($data["stream"]) > 0){
-		$render["IS_STREAM_ACTIVE"] = 1;
-		$render["STREAMER_CHANNEL"] = $configData->ConfigModels["STREAMER_TWITCH_NAME"]->Value;
+	if(isset($data) && count($data["data"]) > 0){
+		if(isset($data["data"][0]["type"]) && $data["data"][0]["type"] == "live"){
+			$render["IS_STREAM_ACTIVE"] = 1;
+			$render["STREAMER_CHANNEL"] = $configData->ConfigModels["STREAMER_TWITCH_NAME"]->Value;
+		}
 	}
 
 	StopTimer("InitStream");
