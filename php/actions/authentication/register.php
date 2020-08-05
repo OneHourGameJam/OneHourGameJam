f0d82141c424837933d0e06ba1007e337baca559
LongarMD <mark.longar@gmail.com>
2020-08-05 13:12:59 +0200

    Separates the login and registration pages (Closes #175) Updates .gitignore with the .idea/ folder

---
+++ php/actions/authentication/register.php
@@ -0,0 +1,14 @@
+<?php
+include_once "authactions.php";
+
+function PerformAction(&$loggedInUser){
+    global $_POST;
+
+    $username = (isset($_POST["un"])) ? $_POST["un"] : "";
+    $password = (isset($_POST["pw"])) ? $_POST["pw"] : "";
+    $loginChecked = false;
+
+    $username = strtolower(trim($username));
+    $password = trim($password);
+    return TryLogin($username, $password, true);
+}
