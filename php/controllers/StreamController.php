08e3c031c09dc3a016ae5c9ea6eaccde48ffd0c3
Marwane Kalam-Alami <marwane.ka@gmail.com>
2020-10-17 13:06:21 +0200

    Create TWITCH_CLIENT_SECRET config key, fix initial twitch stream caching

--- php/controllers/StreamController.php
+++ php/controllers/StreamController.php
@@ -5,26 +5,29 @@ class StreamController{
 		AddActionLog("InitStream");
 		StartTimer("InitStream");
 
-		$streamData = Array();
-
-		$timeDiff = 0;
 		if(file_exists("cache/twitch_stream.json")){
 			//Cache twitch API response
 			$streamData = json_decode(file_get_contents("cache/twitch_stream.json"), true);
-			$timeDiff = time() - intval($streamData["time_last_updated"]);
+			$timeLastUpdated = intval($streamData["time_last_updated"]);
+		} else {
+			$streamData = Array();
+			$timeLastUpdated = 0;
 		}
 
-		if($timeDiff > intval($configData->ConfigModels[CONFIG_TWITCH_API_STREAM_UPDATE_FREQUENCY]->Value)){
+		if(time() - $timeLastUpdated > intval($configData->ConfigModels[CONFIG_TWITCH_API_STREAM_UPDATE_FREQUENCY]->Value)){
 			//Enough time has passed for an update from the API, fetch it.
 
 			//First overwrite the currently saved time_last_updated, so that if there is a lot of load on the twitch API and reponses are slow, only one site user has to wait.
 			$streamData["time_last_updated"] = time();
+
 			file_put_contents("cache/twitch_stream.json", json_encode($streamData));
 
 			//Fetch API response using CURL, because that was the easiest to copy-paste in :)
 			$channelsApi = 'https://api.twitch.tv/helix/streams?user_login=';
 			$channelName = $configData->ConfigModels[CONFIG_STREAMER_TWITCH_NAME]->Value;
 			$clientId = $configData->ConfigModels[CONFIG_TWITCH_CLIENT_ID]->Value;
+			$clientSecret = $configData->ConfigModels[CONFIG_TWITCH_CLIENT_SECRET]->Value;
+
 			$ch = curl_init();
 
 			curl_setopt_array($ch, array(
